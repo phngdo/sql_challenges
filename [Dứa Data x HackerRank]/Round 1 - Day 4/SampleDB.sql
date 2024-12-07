2d33da0d69018d0bd5656480a9d4b3ae885b0d8f
Phuong Le Do <joni.phngdo@gmail.com>
2024-12-08 02:50:21 +0700

    Update SampleDB

--- [Dứa Data x HackerRank]/Round 1 - Day 4/SampleDB.sql
+++ [Dứa Data x HackerRank]/Round 1 - Day 4/SampleDB.sql
@@ -34,24 +34,24 @@ CREATE TABLE dbo.CIF (
 -- insert values:
 INSERT INTO dbo.AREA (AREA_CODE, AREA_NAME)
 VALUES ('A01', N'HỘI SỞ CHÍNH'),
-	     ('A02', N'TÂY BẮC BỘ'),
-	     ('A03', N'ĐÔNG BẮC BỘ'),
-	     ('A04', N'BẮC TRUNG BỘ'),
-	     ('A05', N'NAM TRUNG BỘ');
+	   ('A02', N'TÂY BẮC BỘ'),
+	   ('A03', N'ĐÔNG BẮC BỘ'),
+	   ('A04', N'BẮC TRUNG BỘ'),
+	   ('A05', N'NAM TRUNG BỘ');
 
 INSERT INTO dbo.BRANCH (BRCODE, BRNAME, BRADDRESS, AREA)
 VALUES ('1302001', N'Chi nhánh Đống Đa', N'Tôn Đức Thắng - Đống Đa - HN', 'A01'),
-	     ('1302002', N'Chi nhánh Sở Giao dịch', N'22 Kim Mã Thượng - Hà Nội', 'A02'),
-	     ('1302004', N'Chi nhánh hoàn kiếm', N'Trần Hưng Đạo - Đống Đa - HN', 'A03');
+	   ('1302002', N'Chi nhánh Sở Giao dịch', N'22 Kim Mã Thượng - Hà Nội', 'A02'),
+	   ('1302004', N'Chi nhánh hoàn kiếm', N'Trần Hưng Đạo - Đống Đa - HN', 'A03');
 
 INSERT INTO dbo.CIF (BRCODE, CIF_ID, CIF_NAME, ID_NUMBER, CIF_TYPE)
 VALUES ('1302001', 'CIF02001', N'CÔNG TY TNHH SÁNG TẠO MỚI', '805000457', N'Tổ chức'),
-	     ('1302001', 'CIF02002', N'NGUYỄN VĂN ANH', '6200417055', N'Cá nhân'),
-	     ('1302001', 'CIF02003', N'CÔNG TY TNHH PHÁT TRIỂN THƯƠNG MẠI BẮC NAM', '6200588086', N'Tổ chức'),
-	     ('1302001', 'CIF02004', N'TRẦN THỊ THANH', '6200558854', N'Cá nhân'),
-	     ('1302001', 'CIF02005', N'DOANH NGHIỆP TƯ NHÂN ĐỨC PHÚC', '5706200571', N'Tổ chức'),
-	     ('1302001', 'CIF02006', N'LÊ VĂN HẢI', '62088205', N'Cá nhân'),
-	     ('1302001', 'CIF02007', N'CÔNG TY TNHH THƯƠNG MẠI HOÀNG GIA', '5620516786', N'Tổ chức'),
-	     ('1302002', 'CIF02008', N'CÔNG TY CỔ PHẦN PHÁT TRIỂN ĐẦU TƯ VIỆT NAM', '624075627', N'Tổ chức'),
-	     ('1302002', 'CIF02009', N'HOÀNG NGỌC ANH', '800254255', N'Cá nhân'),
-	     ('1302002', 'CIF02010', N'CÔNG TY CP XÂY DỰNG ĐÔ THỊ VIỆT NAM', '700487565', N'Tổ chức');
+	   ('1302001', 'CIF02002', N'NGUYỄN VĂN ANH', '6200417055', N'Cá nhân'),
+	   ('1302001', 'CIF02003', N'CÔNG TY TNHH PHÁT TRIỂN THƯƠNG MẠI BẮC NAM', '6200588086', N'Tổ chức'),
+	   ('1302001', 'CIF02004', N'TRẦN THỊ THANH', '6200558854', N'Cá nhân'),
+	   ('1302001', 'CIF02005', N'DOANH NGHIỆP TƯ NHÂN ĐỨC PHÚC', '5706200571', N'Tổ chức'),
+	   ('1302001', 'CIF02006', N'LÊ VĂN HẢI', '62088205', N'Cá nhân'),
+	   ('1302001', 'CIF02007', N'CÔNG TY TNHH THƯƠNG MẠI HOÀNG GIA', '5620516786', N'Tổ chức'),
+	   ('1302002', 'CIF02008', N'CÔNG TY CỔ PHẦN PHÁT TRIỂN ĐẦU TƯ VIỆT NAM', '624075627', N'Tổ chức'),
+	   ('1302002', 'CIF02009', N'HOÀNG NGỌC ANH', '800254255', N'Cá nhân'),
+	   ('1302002', 'CIF02010', N'CÔNG TY CP XÂY DỰNG ĐÔ THỊ VIỆT NAM', '700487565', N'Tổ chức');
