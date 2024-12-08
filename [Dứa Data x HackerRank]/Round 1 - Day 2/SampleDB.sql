3ca766b9efcec89a3d1e1434ae241ff1dc1e21c2
Phuong Le Do <joni.phngdo@gmail.com>
2024-12-08 15:26:29 +0700

    Update SampleDB

--- [Dứa Data x HackerRank]/Round 1 - Day 2/SampleDB.sql
+++ [Dứa Data x HackerRank]/Round 1 - Day 2/SampleDB.sql
@@ -19,7 +19,7 @@ CREATE TABLE dbo.Students(
 -- insert value
 INSERT INTO dbo.Students (Full_Name, Age, Major, GPA, Enrollment_Date, Graduation_Year)
 VALUES ('John Doe', 20, 'Computer Sci', 3.6, '2018-08-15', 2024),
-    ('Jane Smith', 22, 'Mathematics', NULL, '2020-09-12', 2023),
-	  ('Emily Johnson', 21, 'Physics', 3.4, NULL, 2024),
-	  ('Michael Brown', 20, 'Computer Sci', 3.9, '2023-02-10', NULL),
-	  ('Olivia White', 23, 'Engineering', 3.2, '2021-11-21', 2023);
+       ('Jane Smith', 22, 'Mathematics', NULL, '2020-09-12', 2023),
+       ('Emily Johnson', 21, 'Physics', 3.4, NULL, 2024),
+       ('Michael Brown', 20, 'Computer Sci', 3.9, '2023-02-10', NULL),
+       ('Olivia White', 23, 'Engineering', 3.2, '2021-11-21', 2023);
