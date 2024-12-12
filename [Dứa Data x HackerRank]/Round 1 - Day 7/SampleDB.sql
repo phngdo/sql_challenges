477377ef314344656e1162a124f1e73051057bcf
Phuong Le Do <joni.phngdo@gmail.com>
2024-12-12 12:32:34 +0700

    Update SampleDB

--- [Dứa Data x HackerRank]/Round 1 - Day 7/SampleDB.sql
+++ [Dứa Data x HackerRank]/Round 1 - Day 7/SampleDB.sql
@@ -24,5 +24,5 @@ VALUES (101, 'North', 'Electronics', '2024-01-10', 5, 200, 0.1, NULL),
 	   (102, 'South', 'Clothing', '2024-02-05', NULL, 50, 0.05, NULL),
 	   (103, 'East', 'Electronics', '2024-03-15', 10, 150, NULL, NULL),
 	   (104, 'North', 'Furniture', '2024-01-25', 3, 500, 0.2, 1200),
-	   (NULL, 'West', 'Electronic', '2024-03-01', 8, 300, 0.15, 2040),
+	   (NULL, 'West', 'Electronics', '2024-03-01', 8, 300, 0.15, 2040),
 	   (105, NULL, 'Clothing', '2024-04-10', 2, 70, 0.1, NULL);
